a85c0a2bd36b4d83afa9ac579d7af7f5c58caef8
Diego Arreceygor <darreceygor@gmail.com>
2021-10-12 21:44:09 -0300

    Segundo commit: se agrego archivo registro.php

---
+++ login/registro.php
@@ -0,0 +1,28 @@
+<?php
+   function renderForm(){
+       echo '
+           <h2>Registro</h2>
+           <form method="POST">
+               <input type="text" name="email" placeholder="Ingrese su email..."/>
+               <input type="password" name="password" placeholder="Ingrese su password..."/>
+               <button>Crear cuenta</button>
+           </form>
+       ';
+   }
+
+   //Imprimir el formulario
+   renderForm();
+
+   //Creo la cuenta cuando venga en el POST
+   if(!empty($_POST['email'])&& !empty($_POST['password'])){
+    $userEmail=$_POST['email'];
+    $userPassword=password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+    
+
+    //Guardo el nuevo usuario en la base de datos
+    $db = new PDO('mysql:host=localhost;'.'dbname=db_users;charset=utf8', 'root', '');
+    $query = $db->prepare('INSERT INTO user (email, password) VALUES (? , ?)');
+    $query->execute([$userEmail,$userPassword]);
+  
+}
