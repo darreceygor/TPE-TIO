8a18857a126bd480bb9fc08a6fb6dd36c024969a
Miqueas Freiberger <miqueasfreiberger@gmail.com>
2021-10-14 17:38:31 -0300

    Agrego tabla de usuarios

---
+++ content/content.php
@@ -0,0 +1,33 @@
+<?php
+
+function renderTable(){
+    echo '
+    <table class="default">
+    <tr>
+  
+      <th>Nombre de Usuario</th>
+  
+      <th>Edad</th>
+  
+  
+    </tr>
+  
+    <tr>
+  
+      <td>Juan Perez</td>
+  
+      <td>40</td>
+  
+  
+    </tr>
+  
+    <tr>
+  
+      <td>Francisco Hernandez</td>
+  
+      <td>25</td>
+  
+    </tr>
+    
+    ';
+}
