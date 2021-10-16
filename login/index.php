43c58591dc2d566033a2f13ec6bd7817e930503f
Bernardo Martinelli <bermartinelli@gmail.com>
2021-10-16 00:54:07 -0300

    creo template home

--- login/index.php
+++ login/index.php
@@ -5,46 +5,7 @@ function renderHome() {
 
 include_once('./templates/header.php') ;
             
-        echo '
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <ul  class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="login">Login</a></li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="registrar">Registro</a></li>
-            </ul>
-        </nav>
-
-        <table  class="table table-bordered">
-        <tr  id="default">
-
-        <th>Nombre de Usuario</th>
-
-        <th>Edad</th>
-
-
-        </tr>
-
-        <tr>
-
-        <td>Juan Perez</td>
-
-        <td>40</td>
-
-
-        </tr>
-
-        <tr>
-
-        <td>Rodrigo Rodriguez</td>
-
-        <td>19</td>
-
-        </tr>
-
-        
-        ' ;
-
+include_once('./templates/home.php') ;
 
 include_once('./templates/footer.php') ;
 }
